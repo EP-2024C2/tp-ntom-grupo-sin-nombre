b79fccd130fd793f7afee004f5e286d00eef13b1
leorodriguez99 <leorodriguez8899@gmail.com>
2024-10-14 23:25:06 -0300

    agrego configuraciones de database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 const express = require("express")
-const routes = require('./routes')
+const routes = require('./routes/index')
+const sequelize = require('../config/database')
+
+
 const app = express()
+
+app.use(express.json())
+
+app.use(routes)
+
+async function startDatabase(){
+  try {
+      await sequelize.sync({force: true})
+      console.log('Base de datos sincronizada')
+
+      console.log('Datos de inicialización cargados correctamente')
+  } catch (error) {
+      console.log('Error al sicronizar o inicializar los datos')
+  }
+} 
+
+startDatabase()
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
