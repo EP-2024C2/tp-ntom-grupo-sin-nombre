ffb3c53ad4089adc7885eeaeb362ec960424c2b7
MartinaFalco <03mleguizamon@gmail.com>
2024-10-21 07:08:07 -0300

    Modificaciones en app

--- src/app.js
+++ src/app.js
@@ -1,37 +1,50 @@
-const express = require("express")
-const routes = require('./routes/index')
+const express = require('express')
+const bodyParser = require('body-parser')
+require('dotenv').config();
+
 const sequelize = require('../config/database')
 const componenteRoute = require("./routes/componente.routes")
 const fabricanteRoute = require("./routes/fabricante.routes")
 const productoRoute = require("./routes/producto.routes")
 
+const db = require('./db/models')
+
+const componenteData = require('./data/componenteData.json')
+const fabricanteData = require('./data/fabricanteData.json')
+const producto_componenteData = require("./data/producto_componenteData.json")
+const producto_fabricanteData = require('./data/producto_fabricanteData.json')
+const productoData = require('./data/productoData.json')
+
 const app = express()
-
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-
-app.use(productoRoute)
 app.use(componenteRoute)
 app.use(fabricanteRoute)
+app.use(productoRoute)
 
+const port = process.env.PORT || 3000
 
-async function startDatabase(){
-  try {
-      await sequelize.sync({force: true})
-      console.log('Base de datos sincronizada')
+app.listen(port, async(req, res)=>{
+  try
+  {
 
-      console.log('Datos de inicialización cargados correctamente')
-  } catch (error) {
-      console.log('Error al sicronizar o inicializar los datos')
+  await db.sequelize.authenticate()
+
+  await db.sequelize.sync({force:true}); 
+
+  componenteData.map(componente => db.Componente.create(componente))
+  fabricanteData.map( fabricante => db.Fabricante.create(fabricante))
+  producto_componenteData.map(producto_componente => db.Producto_Componente.create(producto_componente))
+  producto_fabricanteData.map(producto_fabricante => db.Producto_Fabricante.create(producto_fabricante))
+  productoData.map(producto => db.Producto.create(producto))
+
+  console.log("Listen on "+port)
+  } 
+  catch(error){
+      console.log("Error: "+error)
   }
-} 
 
-startDatabase()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(3000, () => {
-    console.log("Example app listening on port 3000")
-  })
+})
